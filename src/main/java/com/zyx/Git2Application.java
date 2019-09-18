5bd02a5e75e622857759e6cb9b1d12b11e8b13f2
zyx <1034672738@qq.com>
2019-09-18 20:30:45 +0800

    1.1

---
+++ src/main/java/com/zyx/Git2Application.java
@@ -0,0 +1,13 @@
+package com.zyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git2Application.class, args);
+    }
+
+}
